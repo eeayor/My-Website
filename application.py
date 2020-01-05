a430988fcf7549b96130997e6a20dbf1c78fa1b3
logan-lieou <logan.lieou@gmail.com>
2020-01-05 01:08:44 -0600

    ts-react comps

--- application.py
+++ application.py
@@ -1,12 +1,1 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/hello", methods = ["POST"])
-def hello():
-    name = request.form.get("name")
-    return render_template("hello", name = name)
+import json
