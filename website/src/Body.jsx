49caf8745c3bee829868aa781a2061cfbafc72d6
logan <logan@localhost.localdomain>
2020-07-08 18:36:08 -0500

    yes

---
+++ website/src/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Body() {
+	return(
+		<div className="Body">
+			<p>
+              What is this website supposed to be absolutely nothing here to see people!
+			</p>
+		</div>
+  );
+}
+
+export default Body;
